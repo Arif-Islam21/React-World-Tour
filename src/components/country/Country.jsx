185cad10d668223dcc2d0e9d5cba8a1ec4cfd713
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-10 22:09:44 +0600

    first commit

---
+++ src/components/country/Country.jsx
@@ -0,0 +1,13 @@
+import "./country.css";
+
+const Country = ({ country }) => {
+  const { name } = country;
+  console.log(country);
+  return (
+    <div className="country">
+      <h3>Name: {name.common}</h3>
+    </div>
+  );
+};
+
+export default Country;
