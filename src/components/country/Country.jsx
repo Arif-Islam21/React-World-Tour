6a82de2ef4cb83113f4191593ce55215d0d1b98e
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-10 22:37:29 +0600

    recap

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,8 +1,6 @@
 import "./country.css";
-
 const Country = ({ country }) => {
   const { name, flags } = country;
-  console.log(country);
   return (
     <div className="country">
       <h3>Name: {name.common}</h3>
